fc87f31cea239c7fa3d85deccd61974bc593aa16
unknown <sapocalipsys.z@gmail.com>
2021-01-26 14:28:57 +0300

    fix: fix the solution of task

--- src/index.js
+++ src/index.js
@@ -4,10 +4,6 @@ module.exports = function towelSort(matrix) {
     if (matrix === undefined) {
         return [];
     }
-    let flattedArray = matrix.flat(1);
-    if (flattedArray.length === 0) {
-        return [];
-    }
     let arr = [];
     for (var i = 0; i < matrix.length; i++) {
         if (i % 2 === 0) {
